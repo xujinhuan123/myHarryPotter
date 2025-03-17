1007b415ca3d15ab6c0af551395814f391d717e9
xujinhuan123 <390872892@qq.com>
2025-03-17 09:31:41 +0800

    界面没有实现 一下子全输出了

--- main.cpp
+++ main.cpp
@@ -115,13 +115,14 @@ void printResults(const vector<MatchResult> &results)
 int main()
 {
     vector<string> filenames = {
-        "HP2--Harry_Potter_and_the_Chamber_of_Secrets_Book_2_.txt",
-        "HP7--Harry_Potter_and_the_Deathly_Hallows_Book_7_.txt",
-        "J.K. Rowling - HP 3 - Harry Potter and the Prisoner of Azkaban.txt",
-        "J.K. Rowling - HP 4 - Harry Potter and the Goblet of Fire.txt",
-        "J.K. Rowling - HP 6 - Harry Potter and the Half-Blood Prince.txt",
-        "J.K. Rowling - Quidditch Through the Ages.txt",
-        "J.K. Rowling - The Tales of Beedle the Bard.txt"};
+        "./book/HP2--Harry_Potter_and_the_Chamber_of_Secrets_Book_2_.txt",
+        "./book/HP7--Harry_Potter_and_the_Deathly_Hallows_Book_7_.txt",
+        "./book/J.K. Rowling - HP 3 - Harry Potter and the Prisoner of Azkaban.txt",
+        "./book/J.K. Rowling - HP 4 - Harry Potter and the Goblet of Fire.txt",
+        "./book/J.K. Rowling - HP 6 - Harry Potter and the Half-Blood Prince.txt",
+        "./book/J.K. Rowling - Quidditch Through the Ages.txt",
+        "./book/J.K. Rowling - The Tales of Beedle the Bard.txt",
+        "./book/J.K.Rowling - HP 0 - Harry Potter Prequel.txt"};
     auto books = loadBooks(filenames);
 
     string keyword;
