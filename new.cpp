68d6cd28605e7b956466a693b10c99dcc150258c
xujinhuan123 <390872892@qq.com>
2025-03-17 21:52:59 +0800

    报错什么是stoi 理论结果应该是全部显示 输入序号显示具体还没做

--- new.cpp
+++ new.cpp
@@ -3,12 +3,20 @@
 #include <vector>
 #include <fstream>
 #include <algorithm>
+#include <iomanip>
 using namespace std;
+
+struct bookInfo // 需要的信息
+{
+    string context;
+    int page;
+    int chapter;
+};
 
 struct Book
 {
     string name;
-    vector<int> pages;
+    vector<bookInfo> Info;
 };
 
 // 检查字符串是否只包含数字  获取页码
@@ -17,11 +25,67 @@ bool is_only_digit(const std::string &str)
     return std::all_of(str.begin(), str.end(), ::isdigit);
 }
 
+// 找页码
+int getPage(ifstream &file, int lineCount)
+{
+    string line;
+    while (lineCount--) // 到达指定行开始找
+    {
+        getline(file, line);
+    }
+    while (1)
+    {
+        getline(file, line);     // 逐行读取
+        if (is_only_digit(line)) // 如果这一行是数字
+        {
+            return stoi(line); // 转换为int
+        }
+    }
+}
+
+// 找前一行和当前行的内容
+string getContext(ifstream &file, int lineCount)
+{
+    string ans;
+    string line;
+    while (--lineCount != 1 && lineCount != 0) // 定位到上一行
+    {
+        getline(file, line);
+    }
+    getline(file, line); // 读取上一行
+    ans += line;         // 添加到答案
+    ans += "\n";         // 换行
+    getline(file, line); // 读取当前行
+    ans += line;         // 添加到答案
+    return ans;
+}
+
+// 找章节  碰到chapter关键词就+1 ，直到lineCount行
+int getChapter(ifstream &file, int lineCount)
+{
+    string line;
+    int chapter = 0;
+    while (lineCount--)
+    {
+        getline(file, line);
+        if ((line.find("Chapter") != string::npos || line.find("CHAPTER") != string::npos) && line.find("CHAPTER ZERO") == string::npos) // 排除特殊章
+        {
+            chapter++;
+        }
+        else if (line.find("Chapter Zero") != string::npos)
+        {
+            chapter = 0;
+        }
+    }
+    return chapter;
+}
+
+// 加载书籍信息 同时做完查找将结果放入books中
 vector<Book> loadBooks(vector<string> &filenames, string keyword)
 {
     vector<Book> books;
-
-    bool isFind = false; // 标记是否找到关键词 找到关键词以后才需要找页码
+    int page; // 页码
+    // bool isFind = false; // 标记是否找到关键词 找到关键词以后才需要找页码
     for (auto filename : filenames)
     {
         ifstream file(filename);          // 打开每一个文件
@@ -30,13 +94,38 @@ vector<Book> loadBooks(vector<string> &filenames, string keyword)
         while (getline(file, line))       // 读取每一行
         {
             lineCount++;
+            // int index = 0;
             if (line.find(keyword) != string::npos) // 如果这一行包含关键词
             {
-                // 找页码和章节
+                // index = line.find(keyword); // 找到关键词的位置
+                //  找页码和章节和上下文
+                page = getPage(file, lineCount);
+                string context = getContext(file, lineCount);
+                int chapter = getChapter(file, lineCount);
+                books.push_back(Book{filename, {{context, page, chapter}}}); // 添加到books中
             }
         }
+        file.close();
     }
     return books;
+}
+
+void printResults(vector<Book> &books, string keyword)
+{
+    // 打印结果
+    int count = 0;
+    for (int i = 0; i < books.size(); i++)
+    {
+        for (int j = 0; j < books[i].Info.size(); j++)
+        {
+            count++;
+            cout << count << "\t"
+                 << keyword << "\t\t"
+                 << books[i].Info[j].page << "\t"
+                 << books[i].Info[j].chapter << "\t"
+                 << books[i].name << "\n";
+        }
+    }
 }
 
 int main()
@@ -55,7 +144,8 @@ int main()
     cout << "Enter keyword: ";
     cin >> keyword;
 
+    vector<Book> books = loadBooks(filenames, keyword);
     cout << "序号\t" << "人名/地名\t" << "页码\t" << "章节\t" << "书名" << endl;
-
+    printResults(books, keyword);
     return 0;
 }
