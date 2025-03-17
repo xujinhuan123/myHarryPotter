cf2ebd8b6845c2daa83fbdf6835665730a251d6e
xujinhuan123 <390872892@qq.com>
2025-03-17 19:07:11 +0800

    读取每一本书和每一行 找到关键词以后的找页码和章节还没实现

--- new.cpp
+++ new.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <algorithm>
 using namespace std;
+
+struct Book
+{
+    string name;
+    vector<int> pages;
+};
+
+// 检查字符串是否只包含数字  获取页码
+bool is_only_digit(const std::string &str)
+{
+    return std::all_of(str.begin(), str.end(), ::isdigit);
+}
+
+vector<Book> loadBooks(vector<string> &filenames, string keyword)
+{
+    vector<Book> books;
+
+    bool isFind = false; // 标记是否找到关键词 找到关键词以后才需要找页码
+    for (auto filename : filenames)
+    {
+        ifstream file(filename);          // 打开每一个文件
+        string line;                      // 记录每一行的字符串
+        unsigned long long lineCount = 0; // 记录在找第几行
+        while (getline(file, line))       // 读取每一行
+        {
+            lineCount++;
+            if (line.find(keyword) != string::npos) // 如果这一行包含关键词
+            {
+                // 找页码和章节
+            }
+        }
+    }
+    return books;
+}
 
 int main()
 {
@@ -13,7 +49,8 @@ int main()
         "./book/J.K. Rowling - HP 6 - Harry Potter and the Half-Blood Prince.txt",
         "./book/J.K. Rowling - Quidditch Through the Ages.txt",
         "./book/J.K. Rowling - The Tales of Beedle the Bard.txt",*/
-        "./book/J.K.Rowling - HP 0 - Harry Potter Prequel.txt"};
+        "../book/J.K.Rowling - HP 0 - Harry Potter Prequel.txt"};
+    // 注意最后要把../改成./ 因为在vscode中我使用的是C++ compile run 工作台在output文件夹里
     string keyword;
     cout << "Enter keyword: ";
     cin >> keyword;
